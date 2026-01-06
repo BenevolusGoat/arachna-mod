ea5c6b7fdd96dfcc05faabeee9112f6cf541740d
Sanio <BRude602@gmail.com>
2026-01-05 22:52:21 -0700

    Tweaks to tainted unlock code

--- scripts/arachna/characters/tainted_unlock.lua
+++ scripts/arachna/characters/tainted_unlock.lua
@@ -2,6 +2,12 @@ local Mod = ARACHNAMOD
 
 local TAINTED_UNLOCK = {}
 
+local function checkArachnaTaintedLocked()
+	local player = Isaac.GetPlayer()
+	local playerType = player:GetPlayerType()
+	return playerType == Mod.PlayerType.ARACHNA and not Mod.PersistGameData:Unlocked(Mod.Character.ARACHNA_B.ACHIEVEMENT)
+end
+
 function TAINTED_UNLOCK:OnClosetEntry()
 	if not REPENTOGON then return end
 	local level = Mod.Level()
@@ -10,25 +16,20 @@ function TAINTED_UNLOCK:OnClosetEntry()
 	if level:GetStage() == LevelStage.STAGE8 --Home
 		and level:GetCurrentRoomIndex() == 94 --Closet
 		and room:IsFirstVisit()
+		and checkArachnaTaintedLocked()
 	then
-		local player = Isaac.GetPlayer()
-		local playerType = player:GetPlayerType()
-		local unlock_table = Mod.PlayerTypeToCompletionTable[playerType]
-		if unlock_table then
-			local tainted = unlock_table[Mod.CompletionType.TAINTED]
-			if tainted and not Mod.PersistGameData:Unlocked(tainted) then
-				local innerChild = Isaac.FindByType(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, CollectibleType.COLLECTIBLE_INNER_CHILD)[1]
-				local shopKeeper = Isaac.FindByType(EntityType.ENTITY_SHOPKEEPER)[1]
+		local innerChild = Isaac.FindByType(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE,
+			CollectibleType.COLLECTIBLE_INNER_CHILD)[1]
+		local shopKeeper = Isaac.FindByType(EntityType.ENTITY_SHOPKEEPER)[1]
 
-				if innerChild then
-					innerChild:Remove()
-				elseif shopKeeper then
-					shopKeeper:Remove()
-				end
-
-				Isaac.Spawn(EntityType.ENTITY_SLOT, SlotVariant.HOME_CLOSET_PLAYER, playerType, room:GetCenterPos(), Vector.Zero, player)
-			end
+		if innerChild then
+			innerChild:Remove()
+		elseif shopKeeper then
+			shopKeeper:Remove()
 		end
+
+		local player = Isaac.GetPlayer()
+		Mod.Spawn.Slot(SlotVariant.HOME_CLOSET_PLAYER, room:GetCenterPos(), player)
 	end
 end
 
@@ -37,7 +38,7 @@ Mod:AddCallback(ModCallbacks.MC_POST_NEW_ROOM, TAINTED_UNLOCK.OnClosetEntry)
 ---@param slot EntitySlot
 function TAINTED_UNLOCK:CryingTaintedSpriteOnInit(slot)
 	local player = Isaac.GetPlayer()
-	if Mod.PlayerTypeToCompletionTable[player:GetPlayerType()] then
+	if player:GetPlayerType() == Mod.PlayerType.ARACHNA then
 		local sprite = slot:GetSprite()
 		sprite:ReplaceSpritesheet(0, player:GetEntityConfigPlayer():GetTaintedCounterpart():GetSkinPath(), true)
 	end
@@ -47,12 +48,11 @@ Mod:AddCallback(ModCallbacks.MC_POST_SLOT_INIT, TAINTED_UNLOCK.CryingTaintedSpri
 
 ---@param slot EntitySlot
 function TAINTED_UNLOCK:UnlockTainted(slot)
-	local player = Isaac.GetPlayer()
-	local unlock_table = Mod.PlayerTypeToCompletionTable[player:GetPlayerType()]
-	if unlock_table then
+	if checkArachnaTaintedLocked() then
 		local sprite = slot:GetSprite()
-        local tainted = unlock_table[Mod.CompletionType.TAINTED]
-		if tainted and sprite:IsFinished("PayPrize") then
+		local unlock_table = Mod.PlayerTypeToCompletionTable[Mod.PlayerType.ARACHNA]
+		local tainted = unlock_table[Mod.CompletionType.TAINTED]
+		if sprite:IsFinished("PayPrize") then
 			Mod.PersistGameData:TryUnlock(tainted)
 		end
 	end
