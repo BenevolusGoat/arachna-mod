ba44474dbb82a4ec42b60cdb2dbc977208ba9818
BenevolusGoat <brude602@gmail.com>
2026-03-08 19:11:32 -0400

    Arachna Future dialogue

--- scripts/compatibility/patches/future.lua
+++ scripts/compatibility/patches/future.lua
@@ -5,6 +5,9 @@ local function futurePatch()
 	--TODO: temporary dialogue, lol
 	TheFuture.ModdedCharacterDialogue["Arachna"] = {
 		"ack, a spider!",
+		"...oh, you're friendly? that's good.",
+		"sorry, spiders give me the creeps.",
+		"you think there are still spiders in the future?"
 	}
 	TheFuture.ModdedTaintedCharacterDialogue["Arachna"] = {
 		"ack, another spider!",
