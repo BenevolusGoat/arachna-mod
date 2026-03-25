6c506883725ff4bedf174ef4a6c408a5c22b5193
BenevolusGoat <brude602@gmail.com>
2026-03-25 12:50:03 -0400

    Birthcake file WIP

---
+++ scripts/compatibility/patches/birthcake_rebaked.lua
@@ -0,0 +1,49 @@
+local Mod = ARACHNAMOD
+local loader = Mod.PatchesLoader
+
+local ARACHNA = {
+	Name = {
+		en_us = "Arachna's"
+	},
+	PickupQuote = {
+		en_us = ""
+	},
+	AccurateBlurb = {
+		en_us = ""
+	},
+	EIDDesc = {
+		en_us = ""
+	},
+	SpriteName = "gfx/items/trinket/birthcake_arachna.png"
+}
+
+local ARACHNA_B = {
+	Name = {
+		en_us = "Arachna's"
+	},
+	PickupQuote = {
+		en_us = ""
+	},
+	AccurateBlurb = {
+		en_us = ""
+	},
+	EIDDesc = {
+		en_us = ""
+	},
+	SpriteName = "gfx/items/trinkets/birthcake_arachna_b.png"
+}
+
+local function birthcakePatch()
+	local api = BirthcakeRebaked.API
+	api:AddBirthcakePickupText(Mod.PlayerType.ARACHNA, ARACHNA.PickupQuote, ARACHNA.Name)
+	api:AddAccurateBlurbcake(Mod.PlayerType.ARACHNA, ARACHNA.AccurateBlurb)
+	api:AddBirthcakeSprite(Mod.PlayerType.ARACHNA, {SpritePath = ARACHNA.SpriteName})
+	api:AddEIDDescription(Mod.PlayerType.ARACHNA, ARACHNA.EIDDesc)
+
+	api:AddTaintedBirthcakePickupText(Mod.PlayerType.ARACHNA_B, ARACHNA_B.PickupQuote, Mod.PlayerType.ARACHNA, ARACHNA_B.Name, ARACHNA_B.Title)
+	api:AddAccurateBlurbcake(Mod.PlayerType.ARACHNA_B, ARACHNA_B.AccurateBlurb)
+	api:AddBirthcakeSprite(Mod.PlayerType.ARACHNA_B, {SpritePath = ARACHNA_B.SpriteName})
+	api:AddEIDDescription(Mod.PlayerType.ARACHNA_B, ARACHNA_B.EIDDesc)
+end
+
+loader:RegisterPatch("BirthcakeRebaked", birthcakePatch)
